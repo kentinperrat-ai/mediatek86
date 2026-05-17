acaa0b50d65ec8fcad523e97fd95c7265ef17c20
kentinperrat-ai <kentinperrat@gmail.com>
2026-05-17 18:14:23 +0200

    Création complète des maquettes graphiques (Connexion, Personnel, Absences)

--- mediatek86/view/Form1.Designer.cs
+++ mediatek86/view/Form1.Designer.cs
@@ -30,9 +30,9 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnSeConnecter = new System.Windows.Forms.Button();
+            this.txtMdp = new System.Windows.Forms.TextBox();
+            this.txtIdentifiant = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -53,37 +53,37 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "mot de passe";
             // 
-            // button1
+            // btnSeConnecter
             // 
-            this.button1.Location = new System.Drawing.Point(182, 158);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(82, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "se connecter";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSeConnecter.Location = new System.Drawing.Point(182, 158);
+            this.btnSeConnecter.Name = "btnSeConnecter";
+            this.btnSeConnecter.Size = new System.Drawing.Size(82, 23);
+            this.btnSeConnecter.TabIndex = 2;
+            this.btnSeConnecter.Text = "se connecter";
+            this.btnSeConnecter.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // txtMdp
             // 
-            this.textBox1.Location = new System.Drawing.Point(157, 112);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 3;
+            this.txtMdp.Location = new System.Drawing.Point(157, 112);
+            this.txtMdp.Name = "txtMdp";
+            this.txtMdp.Size = new System.Drawing.Size(100, 20);
+            this.txtMdp.TabIndex = 3;
             // 
-            // textBox2
+            // txtIdentifiant
             // 
-            this.textBox2.Location = new System.Drawing.Point(157, 66);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 4;
+            this.txtIdentifiant.Location = new System.Drawing.Point(157, 66);
+            this.txtIdentifiant.Name = "txtIdentifiant";
+            this.txtIdentifiant.Size = new System.Drawing.Size(100, 20);
+            this.txtIdentifiant.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtIdentifiant);
+            this.Controls.Add(this.txtMdp);
+            this.Controls.Add(this.btnSeConnecter);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -97,9 +97,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnSeConnecter;
+        private System.Windows.Forms.TextBox txtMdp;
+        private System.Windows.Forms.TextBox txtIdentifiant;
     }
 }
 
---
+++ mediatek86/view/FrmAbsences.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace mediatek86.view
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvAbsences = new System.Windows.Forms.DataGridView();
+            this.btnAjouterAbsence = new System.Windows.Forms.Button();
+            this.btnSupprimerAbsence = new System.Windows.Forms.Button();
+            this.btnRetour = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAbsences)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvAbsences
+            // 
+            this.dgvAbsences.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAbsences.Location = new System.Drawing.Point(12, 12);
+            this.dgvAbsences.Name = "dgvAbsences";
+            this.dgvAbsences.Size = new System.Drawing.Size(776, 357);
+            this.dgvAbsences.TabIndex = 0;
+            // 
+            // btnAjouterAbsence
+            // 
+            this.btnAjouterAbsence.Location = new System.Drawing.Point(100, 386);
+            this.btnAjouterAbsence.Name = "btnAjouterAbsence";
+            this.btnAjouterAbsence.Size = new System.Drawing.Size(75, 23);
+            this.btnAjouterAbsence.TabIndex = 1;
+            this.btnAjouterAbsence.Text = "Ajouter";
+            this.btnAjouterAbsence.UseVisualStyleBackColor = true;
+            // 
+            // btnSupprimerAbsence
+            // 
+            this.btnSupprimerAbsence.Location = new System.Drawing.Point(375, 386);
+            this.btnSupprimerAbsence.Name = "btnSupprimerAbsence";
+            this.btnSupprimerAbsence.Size = new System.Drawing.Size(75, 23);
+            this.btnSupprimerAbsence.TabIndex = 2;
+            this.btnSupprimerAbsence.Text = "Supprimer";
+            this.btnSupprimerAbsence.UseVisualStyleBackColor = true;
+            // 
+            // btnRetour
+            // 
+            this.btnRetour.Location = new System.Drawing.Point(671, 386);
+            this.btnRetour.Name = "btnRetour";
+            this.btnRetour.Size = new System.Drawing.Size(75, 23);
+            this.btnRetour.TabIndex = 3;
+            this.btnRetour.Text = "Retour";
+            this.btnRetour.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRetour);
+            this.Controls.Add(this.btnSupprimerAbsence);
+            this.Controls.Add(this.btnAjouterAbsence);
+            this.Controls.Add(this.dgvAbsences);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAbsences)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvAbsences;
+        private System.Windows.Forms.Button btnAjouterAbsence;
+        private System.Windows.Forms.Button btnSupprimerAbsence;
+        private System.Windows.Forms.Button btnRetour;
+    }
+}
